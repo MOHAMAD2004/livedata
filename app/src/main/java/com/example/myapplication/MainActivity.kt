42be7f887e20a589de958f9d63f49811e9bef826
mohamad_mahdi <mohmdmhdib@gmail.com>
2020-12-03 07:59:49 +0330

    livedata

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var plus:Button
+    lateinit var result:TextView
+    lateinit var livedata:mainviewmodle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        plus=findViewById(R.id.btn_add)
+        result=findViewById(R.id.txt_result)
+        livedata=ViewModelProvider(this).get(mainviewmodle::class.java)
+        livedata.scope.observe(this, Observer {
+            result.text=it.toString()
+        })
+        plus.setOnClickListener {
+            livedata.add(Integer.parseInt(result.text.toString()))
+        }
     }
+
 }
--- app/src/main/java/com/example/myapplication/mainviewmodle.kt
+++ app/src/main/java/com/example/myapplication/mainviewmodle.kt
@@ -1,4 +1,16 @@
 package com.example.myapplication
 
-class mainviewmodle {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class mainviewmodle:ViewModel() {
+    override fun onCleared() {
+        super.onCleared()
+    }
+     var scope=MutableLiveData<Int>()
+   fun add(a:Int){
+       scope.value=a
+       scope.value=(scope.value)?.plus(1)
+   }
+
 }
